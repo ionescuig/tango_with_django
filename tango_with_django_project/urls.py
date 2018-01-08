4d5c6a70d8eab017a0104bdc11d48c93ebc9b99d
ionescuig <ionescuig@yahoo.com>
2018-01-08 01:07:43 +0000

    Template inheritance - modified html to inherit from base.html

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from rango import views
 
+app_name = 'rango'
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
