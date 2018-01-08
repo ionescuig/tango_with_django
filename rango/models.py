7293c446096f7d2ecd6640827d69d0d9a585ec2c
ionescuig <ionescuig@yahoo.com>
2018-01-08 06:06:46 +0000

    Added option to create users

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.template.defaultfilters import slugify
 
@@ -27,3 +28,16 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+
+    # The additional attributes we wish to include.
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    # Override te __unicode __() method to return out something meaningful!
+    def __str__(self):
+        return self.user.username
