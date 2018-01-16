4fb4a16f37dc6487b3efdc03ff940a482ce02701
ionescuig <ionescuig@yahoo.com>
2018-01-16 03:34:14 +0000

    Added login, logout, restricted page

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,8 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from django.http import HttpResponseRedirect, HttpResponse
 from rango.models import Category, Page
 from django.shortcuts import render
 
@@ -162,3 +166,49 @@ def register(request):
                   {'user_form': user_form,
                    'profile_form': profile_form,
                    'registered': registered})
+
+
+def user_login(request):
+    # If the request is a HTTP POST, try to pull out the relevant information.
+    if request.method == 'POST':
+        # Gather the username and password (from the login form)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Use Django's machinery to attempt to see if the username/password
+        # combination is valid - a User object is returned if it is.
+        user = authenticate(username=username, password=password)
+
+        # If we have a User object, the details are correct.
+        # If None, no user  with matching credentials is found.
+        if user:
+            # Is the account active?
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse('Your account is disabled.')
+        else:
+            # Bad login details.
+            print('Invalid login details: {0}, {1}'. format(username, password))
+            return HttpResponse('Invalid login details supplied.')
+
+    # This request is not a HTTP POST, so display the login form.
+    # This scenario would most likely be a HTTP GET.
+    else:
+        # No context variables to pass to the template system, hence the
+        # blank dictionary object ...
+        return render(request, 'rango/login.html', {})
+
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
+
+
+# Use login_required() decorator to ensure
+# only those logged in can access the view.
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
